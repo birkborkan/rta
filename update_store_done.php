d42dc1c73ebf2e72a55ab3654ba0ea2f28550619
birkborkan <jirard5@gmail.com>
2019-11-01 21:58:59 -0700

    after slove the problems

--- update_store_done.php
+++ update_store_done.php
@@ -11,7 +11,7 @@ if($e_id)
 {   //hid sid eid cno dname pname oldqty pqty newqty ldate1 ldate2 hdate  
     $select_imp = mysql_query("select * from store_imp where eid = '$e_id'");
     $row_imp=mysql_fetch_array($select_imp);
-    /*
+    
     $s_id=$row_imp['sid'];
     $e_id=$row_imp['eid'];
     $c_no=$row_imp['cno'];
@@ -20,20 +20,24 @@ if($e_id)
     $p_type=$row_imp['ptype'];
     $p_qty=$row_imp['pqty'];
     $date1=$row_imp['ldate1'];
-    $date2=$row_imp['ldate2'];*/
+    $date2=$row_imp['ldate2'];
+$select_sqty = mysql_query("select sqty from store where sno = '$s_id'");
+$sqty_old = mysql_fetch_array($select_sqty);
 
+$sqty_new = $sqty_old['sqty'] + $p_qty;
+ 
     if(mysql_num_rows($select_imp)>0){
         $s_date=date('y-m-d');
         $store_update= mysql_query("update store  set
-        sqty= sqty + $p_qty
-        where sno = $s_id ")or die(mysql_error());  //"1لم يتم تحديث"
-                if (isset($store_update)) {
+        sqty= '$sqty_new'
+        where sno = '$s_id' ")or die(mysql_error());   
+                if ($store_update) {
                     $import_update= mysql_query("update store_imp  
                     set stor=1
-                 where eid=$e_id" )or die("2لم يتم تحديث");
+                 where eid = '$e_id'" )or die(mysql_error());
             //history start.................................................
       
-            $s_q=mysql_query(" select * from store where sno = '$s_id'");
+            $s_q=mysql_query(" select * from store where sno = '$s_id'") ;
                 
                   $srow=mysql_fetch_array($s_q);
                  
@@ -56,10 +60,10 @@ if($e_id)
           $insert=mysql_query("insert into history
                         (sid,eid,cno,dname,pname,ptype,pqty,ldate1,ldate2,oldqty,newqty)  values
                         ('$sid','$eid','$c_no','$d_name','$p_name','$p_type','$p_qty','$date1','$date2','$old_qty','$new_qty')")
-                                or die(mysql_error()); //die("خطا في لدخال"); 
+                                or die(mysql_error());  
               
             //history end.........................................................
-                 echo"done";
+                 echo "done";
                 
                 }
                
